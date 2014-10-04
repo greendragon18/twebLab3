fbad629dab0f12c536cbac03993a2f894bf9173d
Nicolas Butticaz <nbutticazleal@gmail.com>
2014-10-04 21:16:59 +0200

    html text field + button

---
+++ public/javascripts/functions.js
@@ -0,0 +1,13 @@
+console.log("bonjour");
+
+function githubSearch() {
+    var textField = document.getElementById('search-field').value;
+
+    console.log(textField);
+}
+
+// Shorthand for $( document ).ready()
+$(function () {
+    console.log("jquery is ready!");
+
+});
