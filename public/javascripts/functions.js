3fc08f513be43ffc1637209d63de6e27cad8ab5c
Nicolas Butticaz <nbutticazleal@gmail.com>
2014-10-05 19:02:33 +0200

    bug avec handelbar

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,13 +1,32 @@
-console.log("bonjour");
+
+
+
+function success(data, textStatus, jqXHR) {
+    console.log(data);
+}
 
 function githubSearch() {
     var textField = document.getElementById('search-field').value;
 
-    console.log(textField);
+    console.log(navigator.userAgent);
+    $.getJSON("http://localhost:3000/github?search=" + textField, function (data, textStatus, jqXHR) {
+        console.log(data);
+
+        //var repo = JSON.parse(data);
+        $("#repo_table").slideToggle;
+
+        var source = $("#repo_table").html();
+        var template = Handlebars.compile(source);
+        //var repo = {repo: [data]};
+        var repo = {repo: [{name: "tutu", owner: "tata", url: "lala", description: "titi", language: "toto", updated: "lili"}]};
+        console.log(repo);
+        $("#repo_table").html(template(repo));
+    });
+
 }
 
 // Shorthand for $( document ).ready()
 $(function () {
     console.log("jquery is ready!");
-
+    $("#repo_table").slideToggle;
 });
